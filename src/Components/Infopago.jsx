48ae480dc7f18881c8f1b8729220bfef14c7aa22
Nicolas Anzola <nicolasanzola200@gmail.com>
2024-05-22 00:15:55 -0500

    Agregar nuevo pago, calcular porcentajes y valores

--- src/Components/Infopago.jsx
+++ src/Components/Infopago.jsx
@@ -1,35 +1,115 @@
 import React from 'react'
 import styles from "./Infopago.module.css" 
-import { LuCircle } from "react-icons/lu";
 import { AiOutlineMinusCircle } from "react-icons/ai";
 import { AiOutlinePlusCircle } from "react-icons/ai";
-import { CiCalendar } from "react-icons/ci";
-import { Input, Label } from 'keep-react'
 import { MdModeEdit } from "react-icons/md";
+import { useState, useEffect } from 'react';
+import { Cursor } from 'phosphor-react';
 
 
+function Infopago({Activo, Deuda}) {
 
+  //<span>{porcent%items.length===0?Math.trunc(porcentaje):porcentaje.toFixed(1)}</span>
+  const [items,setItems] =useState([
+    {
+      detalle:'Anticipo',
+      valor:Deuda,
+      porcentaje:100,
+      fecha:'2023-05-18',
+    }]);
 
+  
+  const[porcent,setPortcent] = useState(100);
 
-
-function Infopago() {
-
-  const items =["hola","primo"]
-  return (
-    <div style={{display:'flex', justifyContent:'center'}}>
-    {items?.map((item,i)=> (
-      <div className={styles.principal} key={i}>
-        <MdModeEdit className={styles.itemicon}style={{padding:3,fontSize:30,textAlign:'center',color:'#FF7A66',border:'solid 3px',borderColor:'#FF7A66',borderRadius:'80%'}} />
-        <input className={styles.inputs} type="text" id="fname" name="fname" placeholder="Anticipo"/>
-        <input className={styles.inputs} type="text" id="fname" name="fname" placeholder="54,5"/>
-        <div className={styles.porcentajes}><AiOutlineMinusCircle /><span>30%</span><AiOutlinePlusCircle /></div>
-        <span>Vence</span>
-        <div><CiCalendar /><span>Fecha</span></div>
-      </div>
+  const afterIndex=(index, newItem,porcentual)=>{
     
-    ))}
-    </div>
-  )
-}
+    const newArray = [...items.slice(0, index + 1), newItem, ...items.slice(index + 1)];
+    setItems(newArray);
 
+    porcentual= porcentual%items.length===0?Math.trunc(porcentual):porcentual.toFixed(1);
+  
+    setItems(prevItems=>prevItems.map(item=>({
+      ...item, porcentaje:porcentual
+    })));
+
+    setItems(prevItems=>prevItems.map(item=>({
+      ...item, valor:(Deuda*(porcentual/100)).toFixed(1)
+    })));
+  }
+  
+  const restarporcentaje = (index,porcentual)=>{
+ 
+    setItems(prevItems=>{
+  const newItem =[...prevItems];
+      if(index >= 0 && index <newItem.leght){
+
+        newItem[index]={
+          ...newItem[index],porcentaje:newItem[index].porcentaje-1
+        };
+        
+      
+      }
+      if (index + 1 <newItem.leght) {
+        
+        newItem[index+1]={
+          ...newItem[index+1],porcentaje:newItem[index + 1].porcentaje+1
+        };
+       
+      }
+      return newItem;
+    })
+
+     
+        
+      
+      
+    
+  }
+
+  
+
+
+
+  const [currentDate, setCurrentDate] = useState('');
+
+  const [ActiveIndex,setActiveIndex]=useState(null)
+  const clicIndex = (index) => {
+    setActiveIndex(index);
+  }
+  
+
+  useEffect(() => {
+    const today = new Date(); 
+    const formattedDate = today.toISOString().split('T')[0];
+    setCurrentDate(formattedDate);
+  }, []);
+  
+
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center'}}>
+        {items?.map((item, index) => (
+          
+          <React.Fragment key={index} >
+            <div className={styles.principal}>
+              <MdModeEdit onClick={() => clicIndex(index)}  className={styles.itemicon} style={{ padding: 3, fontSize: 30, textAlign: 'center', color: ActiveIndex===index ? '#FF7A66': '#FFF' , border: 'solid 3px', borderColor: '#FF7A66', borderRadius: '80%' }} />
+              <input disabled={!Activo && ActiveIndex !== index || !Activo && !ActiveIndex ===index} className={styles.inputs} type="text" id="fname" name="fname" placeholder="Anticipo" defaultValue={item.detalle} />
+              <input disabled={!Activo && ActiveIndex !== index || !Activo && !ActiveIndex ===index} className={styles.inputs} type="text" id="fname" name="fname" placeholder="54,5" defaultValue={item.valor} />
+              <div className={styles.porcentajes}>
+                <AiOutlineMinusCircle onClick={()=>restarporcentaje(index,item.porcentaje)} style={{cursor:'pointer'}}/>
+                <span>{item.porcentaje}</span>
+                
+                <AiOutlinePlusCircle style={{cursor:'pointer'}}/>
+              </div>
+              <span>Vence</span>
+              <span></span>
+              <input type="date" id="start" name="trip-start" value={currentDate} min='' max="" onChange={(e) => setCurrentDate(e.target.value)}/>
+            </div>
+            {index <= (items.length-1) && <AiOutlinePlusCircle onClick={()=>afterIndex(index,{detalle:'pago '+ (index + 1), valor:100,porcentaje:porcent/(items.length+1),fecha:'2023-05-18',},100/(items.length+1))} className={styles.separadorsum} style={{ marginLeft:items.length===1? 70 : 0,marginTop: '50px', fontSize: '35px', color: '#FF7A66' }} />}
+
+          </React.Fragment>
+        ))}
+      </div>
+      )
+  
+}
 export default Infopago
--- src/Components/Pagos.jsx
+++ src/Components/Pagos.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useState } from 'react';
 import styles from "./Pagos.module.css" 
 import { SlArrowDown } from "react-icons/sl";
 import { SlPencil } from "react-icons/sl";
@@ -6,19 +7,29 @@ import Infopago from "./Infopago";
 
 
 
-function Pagos() {
+
+
+function Pagos( Activo) {
+
+ const [isActive,setIsActive] = useState(false);
+ const Activar =() =>{
+  setIsActive(!isActive)
+
+ }
+
+const valorDeuda=182;
   return (
     <div className={styles.principal}>
         <header>
             <div className={styles.Grouptitulo}><h1>Pagos</h1> <span style={{ paddingTop:10 }}><SlArrowDown/></span></div>
             <div style={{gap:10}} className={styles.Grouptitulo}> 
-                <div><span style={{fontSize:10}}>Editar</span> <span style={{ fontSize:8 }}><SlPencil /></span></div> 
-                <div style={{fontSize:14}}><span style={{color:'#94A3B8'}}>Por cobrar</span> <span style={{color:'#0F172A',fontWeight:'bold'}}>182 USD</span></div> 
+                <div onClick={Activar}><span style={{fontSize:10,cursor:'pointer'}}>{isActive?'Guardar':'Editar'}</span> <span style={{ fontSize:8 }}><SlPencil /></span></div> 
+                <div style={{fontSize:14}}><span style={{color:'#94A3B8'}}>Por cobrar</span> <span style={{color:'#0F172A',fontWeight:'bold'}}>{valorDeuda}USD</span></div> 
             </div>
         </header>
 
         <section className={styles.Info}>
-            <Infopago/>
+            <Infopago Deuda={valorDeuda} Activo={isActive} />
         </section>
     </div>
   )
