9e633e1687f46d2526d61032b71dcce6c0387bd7
Nicolas Anzola <nicolasanzola200@gmail.com>
2024-05-23 02:23:59 -0500

    Funcionalidad completa

--- src/Components/Infopago.jsx
+++ src/Components/Infopago.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import styles from "./Infopago.module.css" 
+import Popup from "./Popup";
 import { AiOutlineMinusCircle } from "react-icons/ai";
 import { AiOutlinePlusCircle } from "react-icons/ai";
 import { MdModeEdit } from "react-icons/md";
@@ -15,12 +16,15 @@ function Infopago({Activo, Deuda}) {
       valor:Deuda,
       porcentaje:100,
       fecha:'2023-05-18',
+      pagado:false
     }]);
 
   const[porcent,setPortcent] = useState(100);
   const [currentDate, setCurrentDate] = useState('');
   const [ActiveIndex,setActiveIndex] = useState(null);
   const[ActivePopup,setActivePopup] = useState(false);
+  const [tipopago, setTipopago] = useState('');
+
 
   const afterIndex=(index, newItem,porcentual)=>{
     
@@ -74,37 +78,53 @@ function Infopago({Activo, Deuda}) {
     setItems(prevItems=>{
   const newItem =[...prevItems];
       
-
   if (newItem[index]===newItem[0]) {
     newItem[index+1]={
       ...newItem[index+1],porcentaje:parseInt(newItem[index + 1].porcentaje)-1,valor:Deuda*(newItem[index].porcentaje/100)
     };
     newItem[index]={
       ...newItem[index],porcentaje:parseInt(newItem[index].porcentaje)+1,
-    };
-
-    
-  }
-    
-   else  {
+    }; 
+  }else{
     newItem[index]={
       ...newItem[index],porcentaje:parseInt(newItem[index].porcentaje)+1
     };
     newItem[index-1]={
       ...newItem[index-1],porcentaje:parseInt(newItem[index - 1].porcentaje)-1
     };
-  }
-      
-      return newItem;
+  }    
+   return newItem;
     })
- 
   }
 
   const clicIndex = (index) => {
     setActiveIndex(index);
-    setActivePopup(!ActivePopup)
+   
   }
   
+  const activarPopup = (index) => {
+    setActiveIndex(index)
+    setActivePopup(!ActivePopup)
+    console.log(items);
+  }
+  
+  const realizarPago = (index) => {
+    console.log(index);
+    setItems(prevItems => {
+      const newItem =[...prevItems];
+      newItem[index]={
+        ...newItem[index],pagado:true,tipoPago:tipopago
+      };
+      return newItem;
+    })
+  
+  }
+
+
+  const OpcionPago=(event) => {
+    setTipopago(event.target.value);
+  }
+
   const date =new Date().toISOString().split('T')[0];
   
 
@@ -115,30 +135,32 @@ function Infopago({Activo, Deuda}) {
   }, []);
 
   return (
-    <div style={{ display: 'flex', justifyContent: 'center'}}>
+    <div>
+      <Popup show={ActivePopup} tipoPago={OpcionPago} pagado={()=>realizarPago(ActiveIndex)} onClose={()=>activarPopup()} />
+    <div className={styles.container} style={{ display: 'flex', visibility:ActivePopup?'collapse':'visible',}}>
         {items?.map((item, index) => (
           
-          <React.Fragment key={index} >
-            <div className={styles.principal}>
-              <MdModeEdit onClick={() => clicIndex(index)}  className={styles.itemicon} style={{ padding: 3, fontSize: 30, textAlign: 'center', border: 'solid 3px', borderColor: '#FF7A66', borderRadius: '80%' }} />
+          <React.Fragment key={index}  >
+            <div className={styles.principal} >
+              <MdModeEdit onClick={ ()=>activarPopup(index)}  className={styles.itemicon} style={{ padding: 3, fontSize: 30, textAlign: 'center', border: 'solid 3px', borderColor: '#FF7A66', borderRadius: '80%' }} />
               <input disabled={!Activo } className={styles.inputs} type="text" id="fname" name="fname" placeholder="Anticipo"  value={item.detalle} />
               <input disabled={!Activo} className={styles.inputs} type="text" id="fname" name="fname" placeholder="54,5" value={(Deuda*(item.porcentaje/100).toFixed(2))} />
               <div className={styles.porcentajes}>
                 <AiOutlineMinusCircle onClick={()=>restarporcentaje(index,item.porcentaje)} style={{visibility:!Activo?'hidden':'visible', cursor:'pointer'}}/>
                 <span>{item.porcentaje}%</span>
-                
                 <AiOutlinePlusCircle onClick={() => sumarporcentaje(index,item.porcentaje)} style={{visibility:!Activo?'hidden':'visible', cursor:'pointer'}}/>
               </div>
               <span>Vence</span>
-              <span></span>
               <input type="date" id="start" disabled={!Activo} name="trip-start" value={currentDate} min={date} max="" onChange={(e) => setCurrentDate(e.target.value)}/>
+              <span style={{fontSize:10, color:'#059669',visibility:item.pagado===false?'collapse':'visible'}}>Pagado {new Date().toLocaleDateString()} con {item.tipoPago} </span>
             </div>
             {index <= (items.length-1) && <AiOutlinePlusCircle onClick={()=>afterIndex(index,{detalle:'pago '+ (index + 1), valor:100,porcentaje:porcent/(items.length+1),fecha:'2023-05-18',},100/(items.length+1))} className={styles.separadorsum} style={{ marginLeft:items.length===1? 70 : 0,marginTop: '50px', fontSize: '35px', color: '#FF7A66' }} />}
 
           </React.Fragment>   
         ))}
 
-        <Popup show={ActivePopup} onClose={ActivePopup} />
+        
+      </div>
       </div>
       )
   
--- src/Components/Popup.jsx
+++ src/Components/Popup.jsx
@@ -1,17 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Popup.module.css'; 
+import { GoX } from "react-icons/go";
 
-const Popup = ({ show, onClose }) => {
-  if (!show) {
+
+const Popup = ({ show, onClose, pagado, tipoPago}) => {
+ 
+ 
+    if (!show) {
     return null;
   }
+
+
+
 
   return (
     <div className="popup-overlay">
       <div className="popup-content">
-        <h2>Pop-up Title</h2>
-        <p>This is the content of the pop-up.</p>
-        <button onClick={onClose}>Close</button>
+        <div style={{ width:'100%',display:'flex',justifyContent:'flex-end'}}>
+            <GoX onClick={onClose} style={{ alignItems:'end'}}/>
+        </div>
+        <h2>Pagar</h2>
+        <p>Selecciona metodo de pago</p>
+        <select name="cars" id="dropdown" value={tipoPago} onChange={tipoPago}>
+            <option value="Efectivo">Efectivo</option>
+            <option value="Tarjeta">Tarjeta</option>
+        </select>
+        <div style={{marginTop:10, width:'100%',display:'flex',justifyContent:'center'}}>
+             <button onClick={pagado}>Guardar</button>
+        </div>
+
       </div>
     </div>
   );
